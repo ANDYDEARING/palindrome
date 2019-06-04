fc80f634212933252834bdc482086a507e66cbca
Andy Dearing <andy.dearing@gmail.com>
2019-06-04 15:04:25 -0400

    added palindrome_iterative for code test toggle

---
+++ palindrome_iterative.py
@@ -0,0 +1,22 @@
+def is_palindrome(text):
+    clean_text = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    is_pal = True
+
+    for character in text:
+        if character.lower() in alphabet:
+            clean_text += character.lower()
+
+    i=0
+    while is_pal and i<(len(clean_text)/2):
+        if clean_text[i] != clean_text[(i+1)*(-1)]:
+            is_pal = False
+        i+=1
+    return is_pal
+        
+user_string = input("Type your text here: ")
+is_pal = is_palindrome(user_string)
+if is_pal:
+    print("This is a palindrome.")
+else:
+    print("This is not a palindrome.")
