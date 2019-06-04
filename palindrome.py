1084c06d9319029ab06b8b50e6697f476bc83f88
Andy Dearing <andy.dearing@gmail.com>
2019-06-04 12:55:30 -0400

    makes a 'clean' string for comparison

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,10 @@
 def is_palindrome(text):
+    clean_text = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for character in text:
+        if character.lower() in alphabet:
+            clean_text += character.lower()
+    print(clean_text)
     return False
 user_string = input("Type your text here: ")
 is_pal = is_palindrome(user_string)
