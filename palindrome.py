f8b9e835b8eec0623299e7bee67615bd4f10e759
Andy Dearing <andy.dearing@gmail.com>
2019-06-04 14:40:37 -0400

    started recursive function, now will not clean each time

--- palindrome.py
+++ palindrome.py
@@ -19,4 +19,4 @@ is_pal = is_palindrome(user_string)
 if is_pal:
     print("This is a palindrome.")
 else:
-    print("This is not a palindrome")
+    print("This is not a palindrome.")
--- palindrome_recursive.py
+++ palindrome_recursive.py
@@ -1,5 +1,12 @@
-def is_palindrome(text):
-    is_pal = False
+def is_palindrome(text, is_clean=False):
+    is_pal = True
+    if not is_clean:
+        clean_text = ""
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        for character in text:
+            if character.lower() in alphabet:
+                clean_text += character.lower()
+        text = clean_text
     return is_pal
         
 user_string = input("Type your text here: ")
@@ -7,4 +14,4 @@ is_pal = is_palindrome(user_string)
 if is_pal:
     print("This is a palindrome.")
 else:
-    print("This is not a palindrome")
+    print("This is not a palindrome.")
