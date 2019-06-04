fa7152291fbbb5f82938e42012dd5e64681d0fe8
Andy Dearing <andy.dearing@gmail.com>
2019-06-04 14:11:25 -0400

    got the iterative working in a while loop

--- palindrome.py
+++ palindrome.py
@@ -6,11 +6,17 @@ def is_palindrome(text):
     for character in text:
         if character.lower() in alphabet:
             clean_text += character.lower()
-    n=0
-    while is_pal and n<len(clean_text):
-        n += 1
-        print("working ", n)
-    return False
+
+    # for test_char in clean_text[0:int(len(clean_text)/2)]:
+    #     if test_char != clean_text[(clean_text.index(test_char)+1)*-1]:
+    #         is_pal = False
+    
+    i=0
+    while is_pal and i<(len(clean_text)/2):
+        if clean_text[i] != clean_text[(i+1)*(-1)]:
+            is_pal = False
+        i+=1
+    return is_pal
         
 user_string = input("Type your text here: ")
 is_pal = is_palindrome(user_string)
